0f673b899ce62b94eed3542fabb2b25ffa20cfb5
Céléna Hujol <celena.hujol@etu.unilim.fr>
2026-06-03 12:58:25 +0000

    suppression motion

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, Outlet, RouterProvider, Navigate } from 'react-router-dom'
-import { motion } from "framer-motion"
 import './index.css'
 
 // --- 1. Import de tes deux nouveaux Stores ---
